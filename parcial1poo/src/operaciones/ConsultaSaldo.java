57518c94e269386c7470a6ffddd04846517f501f
josue <josue@josuedebian.josuedavid>
2026-03-13 12:00:54 -0500

    adicion de modulo comsulta saldo

--- parcial1poo/src/operaciones/ConsultaSaldo.java
+++ parcial1poo/src/operaciones/ConsultaSaldo.java
@@ -1,24 +1,26 @@
 package operaciones;
 
-import Cuenta.*;
 
+import usr.Cuenta;
 
+/**
+ * Operación de consulta de saldo.
+ */
 public class ConsultaSaldo extends Operacion {
 
-    public <Cuenta> ConsultaSaldo(Cuenta cuenta, String sucursal) {
+    public ConsultaSaldo(Cuenta cuenta, String sucursal) {
         super("CONSULTA SALDO", 0, cuenta, sucursal);
     }
 
 
     public boolean ejecutar() {
         double saldo = cuenta.consultarSaldo();
-        System.out.printf("%n  💰 Saldo disponible: $%,.0f%n", saldo);
-        System.out.printf("  📋 Tipo de cuenta  : %s%n", cuenta.getTipoCuenta());
-        System.out.printf("  🔢 N° de cuenta    : ****%s%n%n",
+        System.out.printf("%n  Saldo disponible: $%,.0f%n", saldo);
+        System.out.printf("  📋Tipo de cuenta  : %s%n", cuenta.getTipoCuenta());
+        System.out.printf("  🔢N° de cuenta    : ****%s%n%n",
                 cuenta.getNumeroCuenta().substring(Math.max(0, cuenta.getNumeroCuenta().length() - 4)));
         return true;
     }
-
 
     public Comprobante generarComprobante() {
         return new Comprobante(
