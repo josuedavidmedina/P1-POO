c3880eabb16b6082a8c9f30c95a0a51bcfdcc4cf
josue <josue@josuedebian.josuedavid>
2026-03-13 10:59:14 -0500

    segundo commit

---
+++ parcial1poo/src/usr/Cajero.java
@@ -0,0 +1,4 @@
+package usr;
+
+public class Cajero {
+}
---
+++ parcial1poo/src/usr/Cliente.java
@@ -0,0 +1,20 @@
+package usr;
+
+public class Cliente {
+
+    private String id;
+    private String nombre;
+    private String numeroDocumento;
+    private Cuenta cuenta;
+
+
+    public Cliente(String id, String nombre, String numeroDocumento, Cuenta cuenta) {
+        this.id = id;
+        this.nombre = nombre;
+        this.numeroDocumento = numeroDocumento;
+        this.cuenta = cuenta;
+    }
+
+
+    
+}
